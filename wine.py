140a83c5dc2fe2adf1a9f278fc8766c3647c7d9a
Sampath Kumar <135127839+SampathHungund@users.noreply.github.com>
2023-10-03 22:44:10 +0530

    Add files via upload

---
+++ wine.py
@@ -0,0 +1,22 @@
+import numpy as np
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error, r2_score
+
+data = pd.read_csv('C:\\Users\\sampathkumar\\OneDrive\\Desktop\\winequality-red.csv')
+X = data.drop('quality', axis=1)
+y = data['quality']
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+model = LinearRegression()
+model.fit(X_train, y_train)
+y_pred = model.predict(X_test)
+mse = mean_squared_error(y_test, y_pred)
+r2 = r2_score(y_test, y_pred)
+
+print("MSE:",mse)
+print("R2:",r2)
+new_wine_features = np.array([7.0, 0.27, 0.36, 20.7, 0.045, 45.0, 170.0, 1.001, 3.0, 0.45, 8.8]).reshape(1, -1)
+predicted_quality = model.predict(new_wine_features)
+print("Predicted wine quality:",predicted_quality[0])
